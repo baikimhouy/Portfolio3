0e8d2bb452e31e73f86685e0bfeb5de2d26b4c85
baikimhouy <ctin871@gmail.com>
2026-06-09 17:46:55 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   assetsInclude: ["**/*.glb", "**/*.gltf"],
+    build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        }
+      }
+    }
+  }
 });
